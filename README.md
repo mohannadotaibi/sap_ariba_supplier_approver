bffa0f072cae01667dee1c79704b800fe61b53c5
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2024-04-24 09:26:52 +0300

    init - under dev

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # sap_ariba_supplier_approver
+
  work-related electronjs script to speed up suppliers search and approval in SAP Ariba
+
+## Installation
+
+Get it and install compile it like any other electronjs script.
+
+## How to get Token
+
+Token expires very frequently, you will have to manually get it from dev tools of your browser
+The official API of SAP Ariba might not allow you to perform actions that this tool does.
+You will find it in the request headers called `X-Auth-Token`
+
+## Contributions
+
+You should be a geek if you want to contribute to this repo, no need for direction, do the right thing, you should know it.
+
+## License
+
+Unlicensed, do what you want.
---
+++ main.js
@@ -0,0 +1,28 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  mainWindow.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
